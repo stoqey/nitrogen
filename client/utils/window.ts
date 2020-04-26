315437384c5f83e0078c3d3cc009735c8c5b53eb
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-04-26 01:02:57 -0400

    localhost check

--- client/utils/window.ts
+++ client/utils/window.ts
@@ -1,8 +1,9 @@
+import includes from 'lodash/includes';
+
 export const getWebsocketLink = (): string => {
     const url: string = window && window.location && window.location.href;
     const arr = url.split("/");
     const host = arr[2];
-    const proto = arr[0];
-    const protocol = proto === 'https' ? 'wss' : 'ws';
+    const protocol = !includes(host, 'localhost') ? 'wss' : 'ws';
     return `${protocol}://${host}/graphql`;
 }
